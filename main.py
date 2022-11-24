fd5590666460767dbc06ec695b68b2980f576542
Gabor-the-king <giczi.gabor@students.jedlik.eu>
2022-11-24 11:50:39 +0100

    Menü készítése

---
+++ main.py
@@ -0,0 +1,21 @@
+from functions import *
+from os import system
+from data import *
+
+valasztott=''
+while valasztott!='0':
+    if valasztott=='1':
+        pass
+    elif valasztott=='2':
+        system('cls')
+        pass
+    elif valasztott=='3':    
+        system('cls')
+        pass
+    elif valasztott=='4':
+        pass
+    elif valasztott=='5':
+        pass
+    elif valasztott=='6':
+        system('cls')
+        pass
